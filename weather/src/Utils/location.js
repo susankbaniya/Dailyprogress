5ae4ad50076dd37e68f5ad5417d6e80e9f90105c
Prajwla rana <prajwalrana54@gmail.com>
2025-05-20 17:19:52 +0545

    react redux

--- weather/src/Utils/location.js
+++ weather/src/Utils/location.js
@@ -1,27 +1,11 @@
-// export const getCurrentLocation = (callback, onError) => {
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition(  //navigator.geolocation is built in API
-//       (position) => {
-//         const { latitude, longitude } = position.coords;
-//         //console.log(latitude, longitude)
-//         callback(`${latitude},${longitude}`);
-//         fetchData(query);
-//       },
-//       (err) => onError(err.message)
-//     );
-//   } else {
-//     onError("Geolocation is not supported by this browser!");
-//   }
-// };
-
 export const getCurrentLocation = (callback, onError) => {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
+    navigator.geolocation.getCurrentPosition(  //navigator.geolocation is built in API
       (position) => {
         const { latitude, longitude } = position.coords;
-        const query = `${latitude},${longitude}`; // ✅ Define query
-        callback(query);
-        fetchData(query); // ✅ Now it works
+        //console.log(latitude, longitude)
+        callback(`${latitude},${longitude}`);
+        fetchData(query);
       },
       (err) => onError(err.message)
     );
@@ -29,3 +13,5 @@ export const getCurrentLocation = (callback, onError) => {
     onError("Geolocation is not supported by this browser!");
   }
 };
+
+
